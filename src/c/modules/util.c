e69d3ec4bdd0f1e5131ccf3737914cf3bae4c842
Eric Gideon <eric.gideon@redfin.com>
2016-11-23 21:25:13 -0800

    uncomment fctx utils, although they're currently unused

--- src/c/modules/util.c
+++ src/c/modules/util.c
@@ -3,23 +3,23 @@
 // --------------------------------------------------------------------------
 // Utility functions.
 // --------------------------------------------------------------------------
-// #define FIXED_POINT_SCALE 16
-// #define INT_TO_FIXED(a) ((a) * FIXED_POINT_SCALE)
-// #define FIXED_TO_INT(a) ((a) / FIXED_POINT_SCALE)
-// #define FIXED_MULTIPLY(a, b) (((a) * (b)) / FIXED_POINT_SCALE)
+#define FIXED_POINT_SCALE 16
+#define INT_TO_FIXED(a) ((a) * FIXED_POINT_SCALE)
+#define FIXED_TO_INT(a) ((a) / FIXED_POINT_SCALE)
+#define FIXED_MULTIPLY(a, b) (((a) * (b)) / FIXED_POINT_SCALE)
 
-// #define FPoint(x, y) ((FPoint){(x), (y)})
-// #define FPointI(x, y) ((FPoint){INT_TO_FIXED(x), INT_TO_FIXED(y)})
+#define FPoint(x, y) ((FPoint){(x), (y)})
+#define FPointI(x, y) ((FPoint){INT_TO_FIXED(x), INT_TO_FIXED(y)})
 
 
-// static inline FPoint clockToCartesian(FPoint center, fixed_t radius, int32_t angle) {
-// 	FPoint pt;
-// 	int32_t c = cos_lookup(angle);
-// 	int32_t s = sin_lookup(angle);
-// 	pt.x = center.x + s * radius / TRIG_MAX_RATIO;
-// 	pt.y = center.y - c * radius / TRIG_MAX_RATIO;
-// 	return pt;
-// }
+static inline FPoint clockToCartesian(FPoint center, fixed_t radius, int32_t angle) {
+	FPoint pt;
+	int32_t c = cos_lookup(angle);
+	int32_t s = sin_lookup(angle);
+	pt.x = center.x + s * radius / TRIG_MAX_RATIO;
+	pt.y = center.y - c * radius / TRIG_MAX_RATIO;
+	return pt;
+}
 
 int32_t get_angle(int value, int total) {
 	return (value * 360) / total;
